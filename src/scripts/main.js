7cb5501254adfacacfa9778e92e408aa2b551131
guimmotta <aguinaldoammotta@gmail.com>
2025-03-27 14:57:35 -0300

    added carousel feature and styling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 AOS.init();
+
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            768: { slidesPerView: 3 },
+            480: { slidesPerView: 2 },
+            320: { slidesPerView: 1 }
+        }
+    });
+});
